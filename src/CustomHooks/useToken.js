80eb42a4f6a349cc33d656147c68949a7dd6284a
hamzaiqbal2000 <hiqbal.bscs18seecs@seecs.edu.pk>
2022-04-13 15:16:01 +0500

    Using localStorage to Save Data Across Windows

--- src/CustomHooks/useToken.js
+++ src/CustomHooks/useToken.js
@@ -2,14 +2,25 @@ import { useState } from "react";
 
 const useToken = () => {
   const getToken = () => {
-    const tokenString = sessionStorage.getItem("token");
+    //session storage
+    // const tokenString = sessionStorage.getItem("token");
+    // const userToken = JSON.parse(tokenString);
+    // return userToken?.token;
+
+    //local storage
+    const tokenString = localStorage.getItem("token");
     const userToken = JSON.parse(tokenString);
     return userToken?.token;
   };
   const [token, setToken] = useState(getToken());
 
   function saveToken(userToken) {
-    sessionStorage.setItem("token", JSON.stringify(userToken));
+    //session storage
+    // sessionStorage.setItem("token", JSON.stringify(userToken));
+    // setToken(userToken.token);
+
+    //local storage
+    localStorage.setItem("token", JSON.stringify(userToken));
     setToken(userToken.token);
   }
 
